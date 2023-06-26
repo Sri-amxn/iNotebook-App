3d7a12438ce536197adec15e1ed1cba41face901
Aman Srivastava <aman.puggi@gmail.com>
2023-06-27 00:22:35 +0530

    .

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -5,25 +5,28 @@ import AddNote from './AddNote';
 
 const Notes = () => {
   const context = useContext(noteContext);
-  const { notes, fetchNotes } = context;
+  const { notes, fetchNotes, editNote } = context;
 
   useEffect(() => {
     fetchNotes();
+    // eslint-disable-next-line
   }, []);
+  const ref = useRef(null)
+  const refClose = useRef(null)
+  const [note, setNote] = useState({ id: "", etitle: "", edescription: "", etag: "" })
+  
   const updateNote = (currentNote) => {
     ref.current.click()
-    setNote({etitle : currentNote.title, edescription: currentNote.description, etag: currentNote.tag })
+    setNote({ id: currentNote._id, etitle: currentNote.title, edescription: currentNote.description, etag: currentNote.tag })
   }
-  const [note, setNote] = useState({etitle:"", edescription:"", etag:""})
-  const handleClick = (e)=>{
-    console.log('updating the note', note)
-    e.preventDefault();
-}
-const onChange = (e)=>{
-    setNote({...note, [e.target.name] : e.target.value})
-
-}
-  const ref = useRef(null)
+  const handleClick = (e) => {
+    // console.log('updating the note', note);
+    editNote(note.id, note.etitle, note.edescription, note.etag)
+    refClose.current.click()
+  }
+  const onChange = (e) => {
+    setNote({ ...note, [e.target.name]: e.target.value })
+  }
 
   return (
     <>
@@ -43,7 +46,7 @@ const onChange = (e)=>{
                 <div className="mb-3">
 
                   <label htmlFor="title" className="form-label">Title</label>
-                  <input type="text" className="form-control"  value={note.etitle} name="etitle" onChange={onChange} id="etitle" aria-describedby="emailHelp" />
+                  <input type="text" className="form-control" value={note.etitle} name="etitle" onChange={onChange} id="etitle" aria-describedby="emailHelp" />
                 </div>
                 <div className="mb-3">
                   <label htmlFor="description" className="form-label">Description</label>
@@ -51,12 +54,12 @@ const onChange = (e)=>{
                 </div>
                 <div className="mb-3">
                   <label htmlFor="tag" className="form-label">Tag</label>
-                  <input type="text" className="form-control" onChange={onChange}  value={note.etag} id="etag" name="etag" />
+                  <input type="text" className="form-control" onChange={onChange} value={note.etag} id="etag" name="etag" />
                 </div>
               </form>
             </div>
             <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button ref={refClose} type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
               <button type="button" onClick={handleClick} className="btn btn-primary">Update Note</button>
             </div>
           </div>
